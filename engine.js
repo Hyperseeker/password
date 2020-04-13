29d0edf134aaad2313a93bab4e6102f9895d6fe3
Firebrand <firebrandcoding@gmail.com>
2020-04-13 17:56:41 +0700

    spacing

--- engine.js
+++ engine.js
@@ -17,10 +17,12 @@ let Password = {
 	type: "keyboard",
 
 	alphabet: [
+
 		"0", "1", "2", "3", "4", "5", "6", "7",
 		"8", "9", "A", "B", "C", "D", "E", "F",
 		"G", "H", "J", "K", "M", "N", "P", "Q",
 		"R", "S", "T", "V", "W", "X", "Y", "Z"
+
 	],
 
 	solved () { return Password.current.every(cell => cell.solved) },
